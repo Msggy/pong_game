f4135e04079557d88d6123fab2e9645087cb44dc
Ruben_17M <jc2039744@gmail.com>
2025-10-31 19:14:54 +0000

    Alterei a core pq sim

--- pong/pong.py
+++ pong/pong.py
@@ -3,14 +3,14 @@ from random import randint
 
 janela = display.set_mode((700, 500))
 display.set_caption("catch-up")
-janela.fill((255, 145, 228))
+janela.fill((255, 145, 227))
 
 game = True
 clock = time.Clock()
 FPS = 60
 
 while game:
-    janela.fill((255, 145, 228))
+    janela.fill((255, 145, 227))
 
     for e in event.get():
             if e.type == QUIT:
