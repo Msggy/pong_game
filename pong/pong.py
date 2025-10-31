8445d1848a468db4d9da50d2430da9203fb3296a
Maggy <maggyazevedo10@gmail.com>
2025-10-31 18:48:20 +0000

    pong_game

---
+++ pong/pong.py
@@ -0,0 +1,20 @@
+from pygame import *
+from random import randint
+
+janela = display.set_mode((700, 500))
+display.set_caption("catch-up")
+janela.fill((255, 145, 228))
+
+game = True
+clock = time.Clock()
+FPS = 60
+
+while game:
+    janela.fill((255, 145, 228))
+
+    for e in event.get():
+            if e.type == QUIT:
+                game = False
+
+    display.update()
+    clock.tick(FPS)
