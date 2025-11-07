5210a15f2c0074fb1d526c5420cfe5e8f22996e2
Ruben_17M <jc2039744@gmail.com>
2025-11-07 18:51:17 +0000

    Added ball

--- pong/pong.py
+++ pong/pong.py
@@ -11,9 +11,27 @@ game = True
 clock = time.Clock()
 FPS = 60
 
+#Classes
+class GameSprite(sprite.Sprite):
+    def __init__(self, speed, player_image,y,x):
+        self.speed = speed
+        self.image = transform.scale(image.load(player_image),(20,20))
+        self.rect = self.image.get_rect()
+        self.rect.y = y
+        self.rect.x = x
+
+    def reset(self):
+        janela.blit(self.image,(self.rect.x,self.rect.y))
+
+class Ball(GameSprite):
+    def update(self):
+        pass
+    
+ball = Ball(10,"Ball.png",150,200)
 while game:
     janela.blit(background, (0, 0))  # Must include coordinates!
-
+    ball.reset()
+    ball.update()
     for e in event.get():
         if e.type == QUIT:
             game = False
